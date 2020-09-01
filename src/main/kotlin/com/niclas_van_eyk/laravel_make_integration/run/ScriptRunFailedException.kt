ee3a2e175a30bbb1b12e3812cc9419c92a09372d
Niclas van Eyk <niclas.eyk@gmail.com>
2020-09-01 19:54:38 +0200

    Fixed Missing Interpreter Error Handling

--- src/main/kotlin/com/niclas_van_eyk/laravel_make_integration/run/ScriptRunFailedException.kt
+++
@@ -1,6 +0,0 @@
-package com.niclas_van_eyk.laravel_make_integration.run
-
-class ScriptRunFailedException(val run: PHPScriptRunListener): Exception() {
-    val output: String
-        get() = run.texts.joinToString("\n")
-}
