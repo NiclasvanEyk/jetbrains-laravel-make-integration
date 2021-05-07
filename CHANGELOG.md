91417e87ebb75ca7fe86f4e56faadbf0976e481b
Niclas van Eyk <niclas.eyk@gmail.com>
2021-05-07 17:42:43 +0200

    Hopefully fixed build & updated deps

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -102,16 +102,16 @@
     - Remote Interpreters are supported (#1), so now Docker containers or SSH can be used to execute the artisan commands
 - Updated the plugin infrastructure to use Gradle (should have no user-facing changes)
 
-## [v1.1.1]
+## [1.1.1]
 
 - Fixed paths for migrations and factories
 - Notifications should now pop up in case something goes wrong
 - The make commands now execute on another thread, so there should not be any blocking anymore while executing the artisan binary
 
-## [v1.1.0]
+## [1.1.0]
 
 Updated the documentation and package description.
 
-## [v1.0.0]
+## [1.0.0]
 
 This is the initial release, so there are no changes yet.
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,15 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    id("org.jetbrains.kotlin.jvm") version "1.4.32"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "0.4.21"
+    id("org.jetbrains.intellij") version "0.7.3"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-    id("org.jetbrains.changelog") version "0.4.0"
+    id("org.jetbrains.changelog") version "1.1.2"
     // detekt linter - read more: https://detekt.github.io/detekt/kotlindsl.html
-    id("io.gitlab.arturbosch.detekt") version "1.10.0"
+    id("io.gitlab.arturbosch.detekt") version "1.16.0"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
 
 // Import variables from gradle.properties file
@@ -105,11 +105,19 @@ tasks {
         untilBuild(null)
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription(closure {
-            File("./README.md").readText().lines().run {
-                subList(indexOf("<!-- Plugin description -->") + 1, indexOf("<!-- Plugin description end -->"))
-            }.joinToString("\n").run { markdownToHTML(this) }
-        })
+        pluginDescription(
+            closure {
+                File(projectDir, "README.md").readText().lines().run {
+                    val start = "<!-- Plugin description -->"
+                    val end = "<!-- Plugin description end -->"
+
+                    if (!containsAll(listOf(start, end))) {
+                        throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                    }
+                    subList(indexOf(start) + 1, indexOf(end))
+                }.joinToString("\n").run { markdownToHTML(this) }
+            }
+        )
 
         // Get the latest available change notes from the changelog file
         changeNotes(closure {
