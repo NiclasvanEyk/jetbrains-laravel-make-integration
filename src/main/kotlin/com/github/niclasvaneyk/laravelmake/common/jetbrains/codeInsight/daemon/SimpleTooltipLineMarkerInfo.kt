c603ce499cb06bb861c067b9b51f5067ad594bf8
Niclas van Eyk <niclas.eyk@gmail.com>
2022-04-29 21:48:19 +0200

    wip

--- src/main/kotlin/com/github/niclasvaneyk/laravelmake/common/jetbrains/codeInsight/daemon/SimpleTooltipLineMarkerInfo.kt
+++ src/main/kotlin/com/github/niclasvaneyk/laravelmake/common/jetbrains/codeInsight/daemon/SimpleTooltipLineMarkerInfo.kt
@@ -1,6 +1,5 @@
 package com.github.niclasvaneyk.laravelmake.common.jetbrains.codeInsight.daemon
 
-import com.github.niclasvaneyk.laravelmake.plugin.laravel.database.migrations.MigrationStatusLineMarker
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.psi.PsiElement
